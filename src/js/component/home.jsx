c5ea8c9727bc0bd2b398c411316da57319af12ed
avokeys <alexander.ayala.palacin@gmail.com>
2022-02-19 02:11:53 +0000

    finished styling and glow function of traffic lights

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,23 +2,32 @@ import React from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
+import { useState } from "react";
 
 //create your first component
 const Home = () => {
+	const [glowColor, setGlowColor] = useState("red");
 	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="parent">
+			<div className="topTraffic"></div>
+			<div className="trafficBody">
+				<div
+					onClick={() => setGlowColor("red")}
+					className={
+						"light red" + (glowColor === "red" ? " glow" : "")
+					}></div>
+				<div
+					onClick={() => setGlowColor("yellow")}
+					className={
+						"light yellow " +
+						(glowColor === "yellow" ? " glow" : "")
+					}></div>
+				<div
+					onClick={() => setGlowColor("green")}
+					className={
+						"light green " + (glowColor === "green" ? " glow" : "")
+					}></div>
+			</div>
 		</div>
 	);
 };
